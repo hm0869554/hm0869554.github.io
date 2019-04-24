93ed604d7f02edaa245ec51eef73c5e4d3d59337
hm0869554 <46752643+hm0869554@users.noreply.github.com>
2019-04-24 09:16:34 -0500

    a

--- AssignmentEight/sd_mapper.js
+++ AssignmentEight/sd_mapper.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /*
    New Perspectives on HTML5 and CSS3, 7th Edition
@@ -11,12 +11,14 @@
 
 */
 
-var thisTime = new Date("February 3, 2018 3:15:28");
-var timeStr = thisTime.toLocaleString()
-document.getElementById("timeStamp").innerHTML = timeStr
-var thisHour = timeStr.getHours()
-var thisMonth = timeStr.getMonth()
-var mapNum = (2 * thisMonth + thisHour)%24
-imgStr = ("<img src='sd_sky" + mapNum + ".png' />")
-var pic = document.getElementById("planisphere")
-insertAdjacentHTML("afterbegin", imgStr)
+/* setting the time and formatting it to the user's areas date format*/
+var thisTime = new Date();
+var timeStr = thisTime.toLocaleString();
+/* calculating the map to use based on time of year*/
+document.getElementById("timeStamp").innerHTML = timeStr;
+var thisHour = thisTime.getHours();
+var thisMonth = thisTime.getMonth();
+var mapNum = (2 * thisMonth + thisHour) % 24;
+/* Inserting map based on calculations above*/
+var imgStr = ("<img src='sd_sky"+mapNum+".png' />");
+document.getElementById("planisphere").insertAdjacentHTML("afterbegin",imgStr);
