6ee10e43ba289b8db1c74499d836679b7761de41
hm0869554 <46752643+hm0869554@users.noreply.github.com>
2019-08-22 15:25:06 -0500

    a

---
+++ Java/Exersice01/src/Exercise01_01.java
@@ -0,0 +1,14 @@
+
+public class Exercise01_01 {
+	// Name: Hunter Murphy
+	// Date: 8/22/2019
+	// Class: Java (cis-170)
+	// Section: 101
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Computer Science");
+		System.out.println("Programming is fun");
+	}
+
+}
---
+++ Java/Exersice01/src/Exercise01_05.java
@@ -0,0 +1,12 @@
+
+public class Exercise01_05 {
+	// Name: Hunter Murphy
+	// Date: 8/22/2019
+	// Class: Java (cis-170)
+	// Section: 101
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(( 9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+	}
+
+}
