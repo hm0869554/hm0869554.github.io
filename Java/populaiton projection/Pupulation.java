4f025c76d1e057b943b901f82729480c2d301895
hm0869554 <46752643+hm0869554@users.noreply.github.com>
2019-08-22 09:52:27 -0500

    a

---
+++ Java/populaiton projection/Pupulation.java
@@ -0,0 +1,19 @@
+
+public class Pupulation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int p0 = 312032486;
+		int p1;
+		int p2;
+		int p3;
+		int p4;
+		int p5;
+		int Bir;
+		int Dth;
+		int Imm;
+		System.out.println("original population: " + p0);
+		System.out.println("Population after 1 year: " + p1);
+	}
+
+}
