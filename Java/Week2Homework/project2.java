dd1b0bf09e900ef0bd2fe4d7c604dbf99b202969
MURPHY <hm0869554@otc.edu>
2019-08-29 09:51:41 -0500

    Update project2.java

--- Java/Week2Homework/project2.java
+++ Java/Week2Homework/project2.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 
 public class project2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		double years;
+		double days;
+		Scanner input = new Scanner(System.in);
+		
+		double min;
+		min = input.nextInt();
+		
+		System.out.println(min);
+		
 	}
 
+	
 }
