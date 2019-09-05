62f61215329c2072f914e48faa371568c1003b1d
hm0869554 <46752643+hm0869554@users.noreply.github.com>
2019-09-05 15:45:28 -0500

    Update Script.js

--- Web design 2/ImageSwapJava/Script.js
+++ Web design 2/ImageSwapJava/Script.js
@@ -1,4 +1,4 @@
-
+var clicked = 0;
 
 function imageSwap(Image)
 {
@@ -6,16 +6,18 @@ function imageSwap(Image)
 }
 
 document.getElementById('Shift').addEventListener('dblclick', function(){
-    if (document.getElementById('mike').src == "Mike.jpg"){
-        document.getElementById('mike').src = "Cecillia.jpg";
-        document.getElementById('Sully').src = "Boo.jpg";
-        document.getElementById('Randall').src = "Fungus.jpg";
-        document.getElementById('Roz').src = "Waternoose.jpg";
-    }
-    else{
+    if (clicked % 2 == 0){
+    document.getElementById('mike').src = "Cecillia.jpg";
+    document.getElementById('Sully').src = "Boo.jpg";
+    document.getElementById('Randall').src = "Fungus.jpg";
+    document.getElementById('Roz').src = "Waternoose.jpg";
+    clicked = clicked + 1;}
+    else {
         document.getElementById('mike').src = "Mike.jpg";
         document.getElementById('Sully').src = "Sully.png";
-        document.getElementById('Roz').src = "Roz.jpg";
         document.getElementById('Randall').src = "Randall.jpg";
+        document.getElementById('Roz').src = "Roz.jpg";
+        clicked = clicked + 1;
     }
+
 })
