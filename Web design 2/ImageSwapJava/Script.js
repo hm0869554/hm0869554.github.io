a5540df052f9dd96780c1d2d132b86e8291c8679
hm0869554 <46752643+hm0869554@users.noreply.github.com>
2019-09-05 15:28:08 -0500

    Update Script.js

--- Web design 2/ImageSwapJava/Script.js
+++ Web design 2/ImageSwapJava/Script.js
@@ -5,3 +5,17 @@ function imageSwap(Image)
     document.getElementById('Shift').src = Image;
 }
 
+document.getElementById('Shift').addEventListener('dblclick', function(){
+    if (document.getElementById('mike').src == "Mike.jpg"){
+        document.getElementById('mike').src = "Cecillia.jpg";
+        document.getElementById('Sully').src = "Boo.jpg";
+        document.getElementById('Randall').src = "Fungus.jpg";
+        document.getElementById('Roz').src = "Waternoose.jpg";
+    }
+    else{
+        document.getElementById('mike').src = "Mike.jpg";
+        document.getElementById('Sully').src = "Sully.png";
+        document.getElementById('Roz').src = "Roz.jpg";
+        document.getElementById('Randall').src = "Randall.jpg";
+    }
+})
