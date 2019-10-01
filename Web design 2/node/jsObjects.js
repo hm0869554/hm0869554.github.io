2b78b9e140f074db0eefa6edf266c4478d7086f2
MURPHY <hm0869554@otc.edu>
2019-10-01 13:15:39 -0500

    a

--- Web design 2/node/jsObjects.js
+++ Web design 2/node/jsObjects.js
@@ -19,39 +19,66 @@ pen.price = 2.50
 
 //********************************************
 const aurora = {
-    name: "Aurora",
+    name:"Aurora",
     health: 150,
-    Strength: 25
+    Strength: 25,
+    xp: 0
 };
-describe(aurora);
+//describe(aurora);
 //console.log(`${aurora.name} has ${aurora.health} health points and ${aurora.Strength} as strength`)
 // Aurora is harmed by an arrrow for 20 damage - reduce health by 20
 aurora.health -=20;
 
-describe(aurora);
 //console.log(`${aurora.name} has ${aurora.health} health points remaining`);
 
 //Aurora equips a strength necklace - increase strength by 20
 aurora.Strength += 10;
+aurora.xp +=15;
 describe(aurora);
 //console.log(`${aurora.name} has ${aurora.Strength} strength`)
 
 function describe(character)
 {
-    console.log(`${character.name} has ${character.health} health points and ${character.Strength} as strength`)
+    console.log(`${character.name} has ${character.health} health points, ${character.Strength} as strength and ${character.xp} as XP`)
 }
 
-const boris =
-{
-    name: "boris",
-    health: "100",
-    strength: "150",
-    describe(newName){
-        //console.log(`My name is ${this.name}!`)
-        this.name = newName;
-        return `My name is ${this.name}!`;
+//const boris =
+//{
+//    name: "boris",
+//    health: "100",
+//    strength: "150",
+//    describe(newName){
+//        //console.log(`My name is ${this.name}!`)
+//        this.name = newName;
+//        return `My name is ${this.name}!`;
+//    }
+//}
+
+// TODO : Make a dog object
+
+const dog ={
+    name: "Reggie",
+    species: "St. Bernard",
+    weight: "25 lbs",
+    bark: "Woof Woof"
+};
+
+console.log(`${dog.name} is a ${dog.species} dog measuring ${dog.size}`);
+console.log(`Look, a cat! ${dog.name} barks: ${dog.bark}`);
+
+const account = {
+    name : "Alex",
+    balance: 0,
+    credit(credit){
+        //var balanceChange = credit - debit;
+        this.balance += credit;
+    },
+    describe(){
+        console.log(`owner : ${this.name}, balance ${this.balance}`)
     }
-}
+};
 
-
-console.log(boris.describe('john'));
+account.credit(250);
+account.describe();
+account.credit(-80);
+account.describe();
