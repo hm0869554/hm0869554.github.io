2cd638d0f7788fe4a2c997109fc15c830854d596
hm0869554 <46752643+hm0869554@users.noreply.github.com>
2019-10-04 20:01:15 -0500

    a

--- Java/Week4homework/Scratch.java
+++ Java/Week4homework/Scratch.java
@@ -5,17 +5,32 @@ public class Scratch {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-		int longest = 0;
-		int count = 0;
-		while(input.hasNext()) {
-		int length = input.next().length();
-		if(length > longest) {
-		count = 1;
-		longest = length;
-		} else if(length == longest) {
-		count ++;
-		}
-		}
+		double InvestmentAmount;
+		double MonthlyInterestRate;
+		int years;
+		System.out.println("Enter the Investment Amount");
+		InvestmentAmount = input.nextDouble();
+		System.out.println("Enter the Monthly Interest Rate");
+		MonthlyInterestRate = input.nextDouble();
+		System.out.println("Enter the number of years");
+		years = input.nextInt();
+		
+		futureInvestmentValue(InvestmentAmount, MonthlyInterestRate, years);
+		
 	}
+	
+	public static double futureInvestmentValue( double investmentAmount, double monthlyInterestRate, int years) {
+		double InvestmentValue = 0;
+		monthlyInterestRate = monthlyInterestRate/1200;
+		for(int i = 1; i <= years; i++) {
+			InvestmentValue = investmentAmount * Math.pow(1 + monthlyInterestRate, i * 12);
+			InvestmentValue = Math.floor(InvestmentValue *100.0)/100.0;
+			System.out.println(i + " " + InvestmentValue);
+		}
+		
+		
+		return investmentAmount;
+	}
+		
 
 }
