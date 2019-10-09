bf193211b32f9c92b91e60582cc176c34c3b2c67
hm0869554 <46752643+hm0869554@users.noreply.github.com>
2019-10-09 18:47:03 -0500

    a

--- Java/Week4homework/Scratch.java
+++ Java/Week4homework/Scratch.java
@@ -5,31 +5,28 @@ public class Scratch {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-		double InvestmentAmount;
-		double MonthlyInterestRate;
-		int years;
-		System.out.println("Enter the Investment Amount");
-		InvestmentAmount = input.nextDouble();
-		System.out.println("Enter the Monthly Interest Rate");
-		MonthlyInterestRate = input.nextDouble();
-		System.out.println("Enter the number of years");
-		years = input.nextInt();
 		
-		futureInvestmentValue(InvestmentAmount, MonthlyInterestRate, years);
+		int[] zipcodeList = {2,1,5,7,9,27,0,0};
+		int memberId = 29;
+		
+		System.out.println(Membership(zipcodeList));
+		
+		
 		
 	}
 	
-	public static double futureInvestmentValue( double investmentAmount, double monthlyInterestRate, int years) {
-		double InvestmentValue = 0;
-		monthlyInterestRate = monthlyInterestRate/1200;
-		for(int i = 1; i <= years; i++) {
-			InvestmentValue = investmentAmount * Math.pow(1 + monthlyInterestRate, i * 12);
-			InvestmentValue = Math.floor(InvestmentValue *100.0)/100.0;
-			System.out.println(i + " " + InvestmentValue);
+	public static boolean Membership( int[]zipcodeList) {
+		boolean duplicates = false;
+		for(int k = 0; k < zipcodeList.length - 1; k++)
+		{
+			for(int j = zipcodeList.length - 1; j > k; j--)	
+				if(zipcodeList[k] == zipcodeList[j])
+				{
+					duplicates = true;
+				}
+				
 		}
-		
-		
-		return investmentAmount;
+		return duplicates;
 	}
 		
 
