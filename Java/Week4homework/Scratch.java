7b6bd787158e6f525f7855b88f62d9f04bc635dc
hm0869554 <46752643+hm0869554@users.noreply.github.com>
2019-10-12 09:31:44 -0500

    a

--- Java/Week4homework/Scratch.java
+++ Java/Week4homework/Scratch.java
@@ -5,29 +5,50 @@ public class Scratch {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-		
-		int[] zipcodeList = {2,1,5,7,9,27,0,0};
-		int memberId = 29;
-		
-		System.out.println(Membership(zipcodeList));
-		
-		
-		
-	}
-	
-	public static boolean Membership( int[]zipcodeList) {
-		boolean duplicates = false;
-		for(int k = 0; k < zipcodeList.length - 1; k++)
+		int num;
+		System.out.println("Enter list: ");
+		num = input.nextInt();
+		int[] list = new int[num];
+		for(int i = 0; i < num; i++)
 		{
-			for(int j = zipcodeList.length - 1; j > k; j--)	
-				if(zipcodeList[k] == zipcodeList[j])
-				{
-					duplicates = true;
-				}
-				
+			list[i] = input.nextInt();
 		}
-		return duplicates;
+		boolean sort = isSorted(list);
+		if(sort == true)
+		{
+			System.out.println("The list is sorted");
+		}
+		if(sort == false)
+		{
+			System.out.println("The list is not sorted");
+		}
+		
+		
+		
 	}
+		
+	public static boolean isSorted(int[] list)
+	{
+		boolean sort = true;
+		for(int i = 0; i < list.length; i++)
+		{
+			for(int j = i + 1; j < list.length; j++)
+			{
+				if(list[i] > list[j])
+				{
+					sort = false;
+				}
+			}
+		}
+		return sort;
+	}
+			
+		
+		
+		
+		
+		
+	
 		
 
 }
