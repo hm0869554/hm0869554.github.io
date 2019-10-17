295d122268f2408a7c5afc0b60273542f6f798dc
MURPHY <hm0869554@otc.edu>
2019-10-17 12:47:41 -0500

    a

--- Web design 2/API/scripts.js
+++ Web design 2/API/scripts.js
@@ -11,3 +11,16 @@ var obj =
 //conver javascript Object to JSON
 var jsonString = JSON.stringify(obj);
 console.log(jsonString);
+
+//convert JSON to a javascript object
+var backtoObject = JSON.parse(jsonString);
+console.log(backtoObject);
+
+window.addEventListener("load" , initializePage)
+
+
+function initializePage()
+{
+    document.getElementById('movieTitle').innerHTML = backtoObject.movie;
+    document.getElementById('desc').innerHTML = backtoObject.genres.name;
+}
